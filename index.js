9e20e86c0828b1cddeb752313204af9d046ec39d
chelm <christopher.helm@gmail.com>
2015-01-07 14:02:07 -0600

    adding host hook

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
 exports.name = 'ckan';
+// tells koop that this provider uses host registration
+exports.hosts = true;
+
 exports.controller = require('./controller');
 exports.routes = require('./routes');
 exports.model = require('./models/ckan.js');
