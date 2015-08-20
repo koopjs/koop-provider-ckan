483c3876645d9e2e9c56d7277345d82d6994ec5c
Nate Goldman <nate@ngoldman.me>
2015-08-20 14:49:07 -0700

    :white_check_mark: stub model test skeleton

--- test/model-test.js
+++ test/model-test.js
@@ -1,1 +1,19 @@
-// TODO
+var test = require('tape')
+var koop = require('koop/lib')
+var Model = require('../model')
+
+koop.config = {
+  data_dir: __dirname + '/output/'
+}
+
+koop.log = new koop.Logger({logfile: './test.log'})
+koop.Cache = new koop.DataCache(koop)
+koop.Cache.db = koop.LocalDB
+koop.Cache.db.log = koop.log
+
+var ckan = new Model(koop)
+
+test('ckan model', function (t) {
+  t.ok(ckan, 'exists')
+  t.end()
+})
